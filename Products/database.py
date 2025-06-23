eb4947e875b26f82c62bec64ed615cec00589a11
Ninad-Badekar <ninad.badekar@neosoftmail.com>
2025-06-23 19:29:32 +0530

    .

--- Products/database.py
+++ Products/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
-from logger import log
+from Products.logger import log
 
 load_dotenv()
 
